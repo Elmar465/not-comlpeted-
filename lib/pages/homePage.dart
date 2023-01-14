d2c96cffe67ca5693f18a5b8224067f926f19eb1
Elmar465 <Elmar465@gmail.com>
2023-01-15 03:13:21 +0400

    Dio version

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -3,64 +3,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_json/provider/myHomePageProvider.dart';
+// import 'package:flutter_json/provider/myHomePageProvider.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
 
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+
+   var jsonList;
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+  void getData()  async{
+    try {
+      var response = await Dio().get("https://api.metaflix.az/api/filter/slidercontents/AZ");
+      if(response.statusCode == 200){
+        setState(() {
+          jsonList =response.data["result"]  as List;
+        });
+      }else{
+        print(response.statusCode);
+      }
+    } catch (e) {
+      print(e);
+    }
+      
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Json Parsing"),
-      ),
-      body: ChangeNotifierProvider<MyHomePageProvider>(
-        create: (context) => MyHomePageProvider(),
-        child: Consumer<MyHomePageProvider>(
-          builder: (context, provider, child ) {
-            if(provider.data == null ) {
-              provider.getData(context);
-              // ignore: prefer_const_constructors
-              return Center( child: CircularProgressIndicator(),
-              );             
-            }
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          // ignore: prefer_const_constructors
+          return Card(
             // ignore: prefer_const_constructors
-            return SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: SingleChildScrollView(
-                child: DataTable(
-                   // ignore: prefer_const_literals_to_create_immutables
-                   columns: [
-                    // ignore: prefer_const_constructors
-                    DataColumn(label:Text("Verified"),tooltip: "User info" ),
-                    DataColumn(label:Text("First Name"),tooltip: "User info" ),
-                    DataColumn(label:Text("Last Name"),tooltip: "User info" ),
-                    DataColumn(label:Text("Email"),tooltip: "User info" ),
-                    DataColumn(label:Text("Phone"),tooltip: "User info" ),
-
-                    
-                   ], rows: provider.data!.result!.map((data) => 
-                    
-                   DataRow(
-                    cells: [
-                      DataCell(Text(data.id)),
-                      DataCell(Text(data.imdb)),
-                      DataCell(Text(data.name)),
-                      DataCell(Text(data.description)),
-                      DataCell(Text(data.trailers)),
-                    ]
-                   )
-                   ).toList(), 
+            child: Column(
+              children: [
+                ListTile(
+                  // ignore: prefer_const_constructors
+                  title: Text(jsonList[index]["id"]),
+                  // ignore: prefer_const_constructors
+                  subtitle: Text(jsonList[index]["name"]),
+                  // trailing: Text(jsonList[index]["name"]["url"]),
                 ),
-              )
-            );
-          }  
-          
-        )
-    )
+                ListTile(
+                  // ignore: prefer_const_constructors
+                  // title: Text(jsonList[index]["trailers"]),
+                  // ignore: prefer_const_constructors
+                  // subtitle: Text(jsonList[index]["name"]),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: jsonList == null?0:jsonList.length, 
+      ),
     );
   }
 }
